dbfbd05327b73a3d2c04b6df5002970fd92d18cb
Dorsazahedi <123908725+Dorsazahedi@users.noreply.github.com>
2025-08-03 20:09:00 -0700

    Update integration-testing.py

--- booking-service/integration-testing.py
+++ booking-service/integration-testing.py
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 import pytest
-import requests_mock
 from app import app
 
 @pytest.fixture
@@ -9,30 +7,10 @@ def client():
     with app.test_client() as client:
         yield client
 
-
-def test_create_booking_triggers_notification(client):
-    with requests_mock.Mocker() as m:
-        m.post("http://notification-service:5002/notify/email", status_code=200)
-        response = client.post("/bookings", json={"user": "ali", "destination": "Paris"})
-        assert response.status_code == 201
-        assert m.called
-=======
-import pytest
-import requests_mock
-from app import app
-
-@pytest.fixture
-def client():
-    app.config["TESTING"] = True
-    with app.test_client() as client:
-        yield client
-
-
-def test_create_booking_triggers_notification(client):
-    with requests_mock.Mocker() as m:
-        m.post("http://notification-service:5002/notify/email", status_code=200)                  #THIS
-
-        response = client.post("/bookings", json={"user": "ali", "destination": "Paris"})
-        assert response.status_code == 201
-        assert m.called
->>>>>>> f44566b (Final project updates: added order-service, fixed DB, updated README, security)
+def test_booking_flow(client):
+    # Send a valid booking
+    data = {"user": "parisa", "destination": "Toronto"}
+    response = client.post("/bookings", json=data)
+    assert response.status_code == 201
+    assert b"confirmed" in response.data.lower()
+    assert "booking_id" in response.get_json()
